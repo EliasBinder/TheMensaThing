debdfc77f817b65db0270beb12f52d8e2cf997a0
leok <leo.kerschbaumerlk@gmail.com>
2022-11-07 17:09:04 +0100

    structure of the application

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import DashboardScreen from './screens/DashboardScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <DashboardScreen />
       <StatusBar style="auto" />
     </View>
   );
