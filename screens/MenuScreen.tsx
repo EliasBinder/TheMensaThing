dec2de27399604d16e6428337575c58ff2867df7
Emanuele Pippa <epippa@unibz.com>
2022-11-15 15:48:24 +0100

    prova design

--- screens/MenuScreen.tsx
+++ screens/MenuScreen.tsx
@@ -3,3 +3,42 @@ export function MenuScreen() {
         <></>
     )
 }
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: 'rgb(12, 21, 52)',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    cells: {
+        backgroundColor: 'rgb(7, 27, 100)',
+        margin: '5%',
+        align: 'left',
+        borderRadius: '20px',
+    },
+    title: {
+        textColor: 'aliceblue',
+        Textalign: 'center',
+        fontSize: '30px',
+        fontFamily: 'Poppins_Bold',
+    },
+    subtitles: {
+        textColor: 'aliceblue',
+        textAlign: 'left',
+        fontSize: '24px',
+        paddingLeft: '10px',
+    },
+    icon: {
+        width: '40px',
+        height: 'auto',
+        //align: 'right',
+        position: 'relative',
+        margin: '25%',
+    },
+    images: {
+        width: '85px',
+        height: '85px',
+        borderRadius: '30px'
+
+    }
+
+})
