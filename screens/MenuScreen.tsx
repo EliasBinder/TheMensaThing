40ea66d45b96043684badf62c38ae665ad5ae50f
Emanuele Pippa <epippa@unibz.com>
2022-11-22 22:47:08 +0100

    Almost ended menu

--- screens/MenuScreen.tsx
+++ screens/MenuScreen.tsx
@@ -1,65 +1,43 @@
-import {View, StyleSheet, Text, Image} from 'react-native';
-import {globalColors, globalStyles} from '../util/StyleUtil'
+import React from 'react';
+import {ScrollView, StyleSheet, Text, TouchableOpacity, View, Image} from "react-native";
+import {globalColors, globalStyles} from "../util/StyleUtil";
+import {styles} from "../screens/DashboardScreen";
+import {scale} from "../util/ScaleUtil";
+import {Card} from "../components/Card";
+import Tune from "../assets/images/tune";
+import TuneIcon from "../assets/images/tune";
+import {OccupationBar} from "../components/DashboardScreen/OccupationBar";
 
-export function MenuScreen() {
+export function MenuScreen(){
     return (
-        <View style={[styles.container, styles.background]} >
-            <Text style={styles.title}>Today's Menu</Text>
-            <View style={styles.cells} >
-                <Image style={styles.icon} source={require('../assets/images/fork&knife.svg')}></Image>
-                <Text style={[styles.subtitles]}>First Dishes</Text>
-                <Image style={styles.icon} source={require('../assets/images/fork&knife.svg')}></Image>
-                <Text style={styles.subtitles}>Second Dishes</Text>
-            </View>
+        <View style={globalStyles.container}>
+            <ScrollView style={styles.scrollView} contentContainerStyle={styles.scrollViewInner}>
+                <View style={styles.cardRow}>
+                    <Card
+                        title={"Today's Menu"}
+                        icon={<Image style={menu.icon} source={require('../assets/images/fork&knife.svg')}></Image>}
+                        interaction={
+                        <TouchableOpacity style={{marginLeft: 'auto'}}>
+                            <TuneIcon color={"#E6E6E6"} dim={25}/>
+                        </TouchableOpacity>}
+                    >
+                        <View style={styles.cardSection}>
+                            <Text style={styles.cardText}>Main Couse</Text>
+                        </View>
+                        <View style={styles.cardSection}>
+                            <Text style={styles.cardText}>Second Course</Text>
+                        </View>
+                    </Card>
+                </View>
+            </ScrollView>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: "flex-start",
-        alignItems: "center"
-    },
-    background: {
-        backgroundColor:globalColors.primary,
-        //backgroundColor: 'rgb(12, 21, 52)',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    cells: {
-        //backgroundColor: 'rgb(7, 27, 100)',
-        backgroundColor: globalColors.secondary,
-        margin: 10,
-        align: 'left',
-        borderRadius: 20,
-    },
-    title: {
-        
-        color: 'aliceblue',
-        textAlign: 'center',
-        fontSize: 30,
-        fontFamily: 'Poppins_Bold',
-    },
-    subtitles: {
-        color: 'aliceblue',
-        textAlign: 'left',
-        fontSize: 24,
-        paddingLeft: '10px',
-    },
+const menu = StyleSheet.create({
     icon: {
-        width: '40px',
+        dim: 25,
         height: 'auto',
-        //align: 'right',
-        position: 'relative',
-        margin: 30,
     },
-    images: {
-        width: '85px',
-        height: '85px',
-        borderRadius: 30,
-
-    }
-
-})
+});
+ 
