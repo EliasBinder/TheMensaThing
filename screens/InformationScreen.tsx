542f79a25b2a8f757e8d9c3cbe73b215e9cdbb77
leok <leo.kerschbaumerlk@gmail.com>
2022-11-23 11:02:19 +0100

    information screen with icons from new custom icon set

--- screens/InformationScreen.tsx
+++ screens/InformationScreen.tsx
@@ -7,26 +7,38 @@ import {List} from "../components/List";
 const createListItem = (iconName: string, text: string) => {
     return (
         <View style={styles.menuItem}>
-            <View>{getIcon({label: iconName, color: globalColors.accent, dim: {width: 29, height: 29}})}</View>
+            <View>{getIcon({label: iconName, color: globalColors.accent, size: 29})}</View>
             <Text style={styles.menuItemText}>{text}</Text>
         </View>
     )
 }
 
-const getIcon = ({label, color, dim}: {label: string, color: string, dim: {width:number, height: number}}) => {
+const getIcon = ({label, color, size}: {label: string, color: string, size:number}) => {
     switch (label) {
         case 'leaf':
-            return <Icon name={'leaf'} size={29} color={globalColors.accent}/>
+            return <Icon name={'leaf'} size={size} color={color}/>
         case 'frozen':
-            return <Icon name={'frozen'} size={29} color={globalColors.accent}/>
+            return <Icon name={'frozen'} size={size} color={color}/>
         case 'celery':
-            return <Icon name={'celery'} size={29} color={globalColors.accent}/>
+            return <Icon name={'celery'} size={size} color={color}/>
         case 'milk':
-            return <Icon name={'milk'} size={29} color={globalColors.accent}/>
+            return <Icon name={'milk'} size={size} color={color}/>
         case 'wine':
-            return <Icon name={'wine'} size={29} color={globalColors.accent}/>
-        case 'mustard':
-            return <Icon name={'mustard'} size={29} color={globalColors.accent}/>
+            return <Icon name={'wine'} size={size} color={color}/>
+        case 'fish':
+            return <Icon name={'fish'} size={size} color={color}/>
+        case 'egg':
+            return <Icon name={'egg'} size={size} color={color}/>
+        case 'pig':
+            return <Icon name={'pig'} size={size} color={color}/>
+        case 'soy_beans':
+            return <Icon name={'soy_beans'} size={size} color={color}/>
+        case 'wheat':
+            return <Icon name={'wheat'} size={size} color={color}/>
+        case 'sesame':
+            return <Icon name={'sesame'} size={size} color={color}/>
+        case 'cashew_nut':
+            return <Icon name={'cashew_nut'} size={size} color={color}/>
     }
 }
 
@@ -34,10 +46,16 @@ export function InformationScreen() {
     const listItems = [
         createListItem('leaf', 'Vegan'),
         createListItem('frozen', 'Contains frozen ingredients'),
-        createListItem('celery', 'Celery'),
-        createListItem('milk', 'Contains milk'),
-        createListItem('wine', 'contains sulphur'),
-        createListItem('mustard', 'contains mustard'),
+        createListItem('celery', 'Celery and products thereof'),
+        createListItem('milk', 'Milk and products thereof'),
+        createListItem('wine', 'Sulphur dioxide and sulphites > 10mg/l'),
+        createListItem('fish', 'Fish and products thereof'),
+        createListItem('egg', 'Egg and products thereof'),
+        createListItem('pig', 'Pork and products thereof'),
+        createListItem('soy_beans', 'Soybeans and products thereof'),
+        createListItem('wheat', 'Cereals containing gluten'),
+        createListItem('sesame', 'Sesame seeds and products thereof'),
+        createListItem('cashew_nut', 'Contains nuts'),
     ];
 
     return (
