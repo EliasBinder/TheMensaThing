5408b810b0ce2a2117028b29f7f92a095a1d95a9
Elias Binder <ebinder@unibz.it>
2022-12-06 18:39:53 +0100

    Add microsaft authentication

--- App.tsx
+++ App.tsx
@@ -9,25 +9,7 @@ import DashboardScreen from "./screens/DashboardScreen";
 import BottomNavScreen from "./screens/BottomNavScreen";
 import {createIconSetFromIcoMoon} from "@expo/vector-icons";
 
-import { initializeApp } from 'firebase/app';
-import { getAuth, onAuthStateChanged, User, OAuthProvider } from 'firebase/auth';
 import {AuthUtil, AuthUtilType} from "./util/AuthUtil";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBfipLKf2Zo4KZ8D_6GsHdzslsaZB2gPNE",
-  authDomain: "unibz-mensapp.firebaseapp.com",
-  projectId: "unibz-mensapp",
-  storageBucket: "unibz-mensapp.appspot.com",
-  messagingSenderId: "149619072455",
-  appId: "1:149619072455:web:a2ccf007c3b099d61a8de2"
-};
-const firebaseAuthProvider = new OAuthProvider('microsoft.com');
-firebaseAuthProvider.setCustomParameters({
-  prompt: 'consent',
-  tenant: '92513267-03e3-401a-80d4-c58ed6674e3b'
-});
-const firebaseApp = initializeApp(firebaseConfig);
-const firebaseAuth = getAuth();
 
 
 export default function App() {
--- components/ProfileScreen/NotLoggedIn.tsx
+++ components/ProfileScreen/NotLoggedIn.tsx
@@ -2,41 +2,36 @@ import {View, StyleSheet, Text} from "react-native";
 import {globalColors, globalStyles} from "../../util/StyleUtil";
 import {scale} from "../../util/ScaleUtil";
 import {BigButton} from "../BigButton";
+// @ts-ignore
+import {AzureInstance, AzureLoginView} from '@shedaltd/react-native-azure-ad-2';
 
-import { initializeApp } from 'firebase/app';
-import { getAuth, onAuthStateChanged, User, OAuthProvider, signInWithRedirect, getRedirectResult } from 'firebase/auth';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBfipLKf2Zo4KZ8D_6GsHdzslsaZB2gPNE",
-  authDomain: "unibz-mensapp.firebaseapp.com",
-  projectId: "unibz-mensapp",
-  storageBucket: "unibz-mensapp.appspot.com",
-  messagingSenderId: "149619072455",
-  appId: "1:149619072455:web:a2ccf007c3b099d61a8de2"
+const CREDENTIAILS = {
+    client_id: '6d082866-1528-4b93-a663-04de0d1e7b45',
+    client_secret: '5029e8f0-cc4e-49b5-8417-d93e1f665a12',
+    redirect_uri: 'https://login.microsoftonline.com/common/oauth2/nativeclient',
+    scope: 'User.ReadBasic.All Mail.Read offline_access'
 };
-const firebaseAuthProvider = new OAuthProvider('microsoft.com');
-firebaseAuthProvider.setCustomParameters({
-  prompt: 'consent',
-  tenant: '92513267-03e3-401a-80d4-c58ed6674e3b'
-});
-const firebaseApp = initializeApp(firebaseConfig);
-const firebaseAuth = getAuth();
+
+const Instance = new AzureInstance(CREDENTIAILS);
 
 
 export function NotLoggedIn({navigation, route}: {navigation: any, route: any}) {
     return (
-        <View style={[globalStyles.container, styles.container]}>
-            <Text style={styles.message}>Please log in to use all the features of this app</Text>
-            <BigButton text={'Login'} onPress={() => {
-                console.log(firebaseAuth);
-                signInWithRedirect(firebaseAuth, firebaseAuthProvider);
-                getRedirectResult(firebaseAuth).then(creds => {
-                    console.log(creds);
-                }).catch(err => {
-                    console.warn(err);
-                });
-            }} style={styles}/>
-        </View>
+        <>
+            <AzureLoginView
+                azureInstance={Instance}
+                loadingMessage="Requesting access token"
+                onSuccess={() => console.log("Success")}
+                onCancel={() => console.log("Cancel")}
+                onError={() => console.log("Error")}
+            />
+            <View style={[globalStyles.container, styles.container]}>
+                <Text style={styles.message}>Please log in to use all the features of this app</Text>
+                <BigButton text={'Login'} onPress={() => {
+
+                }} style={styles}/>
+            </View>
+        </>
     )
 }
 
