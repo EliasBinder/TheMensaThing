a9ab32b151f9a152c09315589238fce1ecbc60cd
Elias Binder <ebinder@unibz.it>
2022-12-07 12:44:18 +0100

    Decrease amount of warning messages

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -10,13 +10,27 @@ export function LoginScreen({navigation, route}:{navigation: any, route: any}) {
     return (
         <>
             <StackNavigationHeader title={"Login"} navigation={navigation} route={route}/>
-            <AzureLoginView
-                azureInstance={AZURE_INSTANCE}
-                loadingMessage="Requesting access token"
-                onSuccess={() => navigation.goBack()}
-                onCancel={() => navigation.goBack()}
-                onError={() => console.log("Error")}
-            />
+            <View style={{
+                flex: 1,
+                backgroundColor: '#33ff41',
+                paddingTop: -50,
+                alignItems: 'flex-start',
+                justifyContent: 'flex-start'
+            }}>
+                <AzureLoginView
+                    azureInstance={AZURE_INSTANCE}
+                    loadingMessage="Requesting access token"
+                    onSuccess={() => navigation.goBack()}
+                    onCancel={() => navigation.goBack()}
+                    onError={() => console.log("Error")}
+                    style={{
+                        marginTop: -10,
+                        width: '100%',
+                        height: '100%',
+                        selfAlign: 'flex-start',
+                    }}
+                />
+            </View>
         </>
     )
 }
