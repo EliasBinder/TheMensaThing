ef7a8a94500ad1e7ad543a5821368ad464461788
Elias Binder <ebinder@unibz.it>
2023-02-02 04:21:42 +0100

    Add suggestion

--- components/DashboardScreen/Suggestion.tsx
+++ components/DashboardScreen/Suggestion.tsx
@@ -1,11 +1,59 @@
 import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {OccupationBar} from "./OccupationBar";
 import {Card} from "../Card";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {DishItem} from "../MenuScreen/DishItem";
 import {Icon} from "../Icon";
+import {usePreferredDishes} from "../../hooks/usePreferredDishes";
+import {usePreferredLocation} from "../../hooks/usePreferredLocation";
+import {useMenu} from "../../hooks/useMenu";
+import {menuItem} from "../../model/menu/menuItem";
 
 export function Suggestion() {
+
+    const [preferredDishes, setPreferredDishes] = usePreferredDishes();
+    const [location, setLocation] = usePreferredLocation();
+    const [menu, setMenu] = useMenu("BZ");
+
+    const [suggestion, setSuggestion] = useState(null as menuItem | null);
+
+    useEffect(() => {
+        if (location == '') return;
+        setMenu(location)
+    }, [location]);
+
+    useEffect(() => {
+        //Check main courses for matching dishes with preferred dishes
+        if (menu == null) return;
+        let matchingMainCourses = menu.mainCourses.filter((item) => {
+            return preferredDishes.filter((_item) => { return _item.name == item.name }).length > 0;
+        });
+        if (matchingMainCourses.length > 0) {
+            setSuggestion(matchingMainCourses[0]);
+            return;
+        }
+
+        //check first courses for matching dishes with preferred dishes
+        let matchingFirstCourses = menu.firstCourses.filter((item) => {
+            return preferredDishes.filter((_item) => { return _item.name == item.name }).length > 0;
+        });
+        if (matchingFirstCourses.length > 0) {
+            setSuggestion(matchingFirstCourses[0]);
+            return;
+        }
+
+        //check pizzas for matching dishes with preferred dishes
+        let matchingPizzas = menu.pizza.filter((item) => {
+            return preferredDishes.filter((_item) => { return _item.name == item.name }).length > 0;
+        });
+        if (matchingPizzas.length > 0) {
+            setSuggestion(matchingPizzas[0]);
+            return;
+        }
+
+        setSuggestion(null);
+    }, [menu, preferredDishes]);
+
     return (
         <Card
             title={"Suggestion"}
@@ -13,7 +61,12 @@ export function Suggestion() {
             index={2}
         >
             <View style={styles.cardSection}>
-                {/*<DishItem iconUrl={"https://picsum.photos/400/300"} title={"Delicious meal"} eatingHabitsAttribs={[1,2,3,6]}/>*/}
+                {
+                    suggestion ?
+                        <DishItem _menuItem={suggestion}/>
+                    :
+                        <Text style={styles.loadingText}>No suggestion available</Text>
+                }
             </View>
         </Card>
     )
@@ -26,5 +79,10 @@ const styles = StyleSheet.create({
         justifyContent: "flex-start",
         alignItems: "flex-start",
         alignSelf: "stretch"
+    },
+    loadingText: {
+        color: '#fff',
+        fontSize: 15,
+        fontFamily: 'Poppins'
     }
 });
